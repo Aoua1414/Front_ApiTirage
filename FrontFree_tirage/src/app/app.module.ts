0e952d886e81acd1bab1389875ad10225569ef74
adkonte <alykonte19@gmail.com>
2022-09-10 22:55:51 +0000

    mise en jour

--- FrontFree_tirage/src/app/app.module.ts
+++ FrontFree_tirage/src/app/app.module.ts
@@ -7,27 +7,23 @@ import { TirageComponent } from './tirage/tirage.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ListeTireComponent } from './liste-tire/liste-tire.component';
 import { PostulantImporterComponent } from './postulant-importer/postulant-importer.component';
-<<<<<<< HEAD
 import { AccueilComponent } from './accueil/accueil.component';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
-=======
+
 import { PageDesListesComponent } from './page-des-listes/page-des-listes.component';
 import { PostulantsComponent } from './postulants/postulants.component';
 
->>>>>>> b903ab218ca71f062b22a94ac17f17cdf29deba4
+
 @NgModule({
   declarations: [
     AppComponent,
     TirageComponent,
     ListeTireComponent,
     PostulantImporterComponent,
-<<<<<<< HEAD
-    AccueilComponent
-=======
+    AccueilComponent,
     PageDesListesComponent,
     PostulantsComponent
->>>>>>> b903ab218ca71f062b22a94ac17f17cdf29deba4
   ],
   imports: [
     BrowserModule,
