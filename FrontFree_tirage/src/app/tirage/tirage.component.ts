4d0e370074472861a0b6df066454c92846a92854
adkonte <alykonte19@gmail.com>
2022-09-19 17:17:51 +0000

    commit avant l'import du fichier

--- FrontFree_tirage/src/app/tirage/tirage.component.ts
+++ FrontFree_tirage/src/app/tirage/tirage.component.ts
@@ -34,4 +34,26 @@ onSubmit(){
    
    })
  }
+ //Pour creer un popup
+ //private modaService: NgbModal
+//  closeResult!: string;
+
+// open(content:any) {
+//   this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+//     this.closeResult = Closed with: ${result};
+//   }, (reason) => {
+//     this.closeResult = Dismissed ${this.getDismissReason(reason)};
+//   });
+// }
+
+// private getDismissReason(reason: any): string {
+//   if (reason === ModalDismissReasons.ESC) {
+//     return 'by pressing ESC';
+//   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+//     return 'by clicking on a backdrop';
+//   } else {
+//     return  with: ${reason};
+
+// }
+// }
 }
